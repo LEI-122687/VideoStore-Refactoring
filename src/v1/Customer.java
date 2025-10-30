c74ae8949023d0403487b72ed7ffb2a309ff997a
Junzun <jhenu@iscte-iul.pt>
2025-10-30 12:18:15 +0000

    v1

--- src/v1/Customer.java
+++ src/v1/Customer.java
@@ -41,12 +41,10 @@ public class Customer
 			if ((each.getMovie().getPriceCode() == Movie.Code.NEW_RELEASE) && each.getDaysRented() > 1)
 				frequentRenterPoints++;
 
-			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
 			totalAmount += thisAmount;
 		}
 
-		// add footer lines
 		result += "Amount owed is " + totalAmount + "\n";
 		result += "You earned " + frequentRenterPoints + " frequent renter points";
 		return result;
